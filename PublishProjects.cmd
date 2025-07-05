23921475f6ad2f9359fec5ff25490f1191592d5d
Stefan Tucker <stefan.tucker@outlook.com>
2025-07-04 23:35:33 -0500

    build(version): release 2.3.4

--- PublishProjects.cmd
+++ PublishProjects.cmd
@@ -2,6 +2,20 @@
 rem
 rem --output "C:\Path\publish\"
 rem
+
+setlocal
+
+:: Prompt for version number if not passed as an argument
+if "%~1" == "" (
+    echo Usage: %~nx0 ^<version^>
+    exit /b 1
+)
+
+set VERSION=%~1
+
+dotnet pack LicenseManager_12noon.Client\LicenseManager_12noon.Client.csproj --nologo -p:Version=%VERSION% --configuration Release --runtime win-x64 --output C:\VSIntermediate\LicenseManagerX\publish\
 dotnet publish LicenseManagerX\LicenseManagerX.csproj --property:PublishProfile=FolderProfile
 dotnet publish LicenseManagerX.Console\LicenseManagerX.Console.csproj --property:PublishProfile=FolderProfile
 dotnet publish LicenseManagerX_Example\LicenseManagerX_Example.csproj --property:PublishProfile=FolderProfile
+
+endlocal
